9bd28a9a51e5a86151f972ed7bd4e941b029e947
DaYmiHn <sawi.denis@yandex.ru>
2019-11-18 21:45:12 +0300

    1111

--- index.php
+++ index.php
@@ -16,12 +16,35 @@ if(isset($_GET['word']) && isset($_GET['perevod'])){
 	<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 </head>
 <body>
-	<div class="container">
+	
+	<header id="header" class="material">
+		<div></div>
+		<div class="header_item container"><p>ГЛАВНАЯ</p></div>
+		<div class="header_item gramm_head_item"><p>УРОКИ</p></div>
+		<div class="header_item test_head_item"><p>ТЕСТЫ</p></div>
+		<div class="header_item podbor_head_item"><p>МОЙ ПРОФИЛЬ</p></div>
+		<div></div>
+	</header>
+	<div id="test_head_item">
+		<center><img src="src/2.png" alt=""><br>
+		<h1 style="font-family: Arial; font-weight: 900;">ЧИПСИКИ</h1></center>
+	</div>
+	<div id="gramm_head_item">
+
+		<center><img src="src/1.jpg" alt=""><br>
+		<h1 style="font-family: Arial; font-weight: 900;">ХОБА</h1></center>
+	</div>
+	<div id="container">
 		<div class="gramm-block material">
-			hsdf
+			<iframe width="100%" height="100%" src="https://www.youtube.com/embed/videoseries?list=PLD6SPjEPomasIFEwzlWscfw8qtwiDsDgL" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 		</div>
 		<div class="acount-block material">
-			acount
+			<h2>А вы знали?</h2><br>
+			Что было бы отлично, если бы вы зашли в свой аккаунт <br>
+			<div class="buttons">
+				<div class="button" id="login">войти</div>
+				<div class="button" id="registration">регистрация</div>
+			</div>
 		</div>
 		<div class="history-block material">
 			<h1>История слов:</h1> 
@@ -33,13 +56,24 @@ if(isset($_GET['word']) && isset($_GET['perevod'])){
 			</div>
 		</div>
 		<div class="slovar-block material">
-			slovar
+			<h2>Словарь</h2>
+			<span>Просто введите слово <br> на английском языке</span><br><br>
+			<input type="text" class="textbox_slovar"><br>
+			
+			<div class="output">
+				<blockquote>
+					<p>Не ошибается лишь тот, кто ничего не делает! 
+					Не бойтесь ошибаться&nbsp;— бойтесь повторять ошибки.</p>
+					<p>Теодор Рузвельт</p>
+				</blockquote>
+			</div>
 		</div>
 		<div class="blog-block material">
 			<?php include 'script/graber_statey.php'; ?>
 		</div>
+		
 	</div>
-
+<script src="js/script.js"></script>
 <script>
 	let timerId = setInterval(function(){$("#hyinya").load(location.href + " #hyinya");} , 2000)
 </script>
---
+++ js/script.js
@@ -0,0 +1,31 @@
+var from_vkladka = '#container';
+$('.header_item').click(function(){
+	var to_vkladka = $(this).attr('class').split(' ')[1];
+	if(from_vkladka == "#"+to_vkladka)return;
+	$(from_vkladka).slideUp(300);
+	$("#"+to_vkladka).slideDown(300);
+	from_vkladka = "#"+to_vkladka;
+});
+
+
+$('#login').click(function(){
+	$(this).css({"grid-column": "1/3"});
+	$('#registration').remove();
+});
+
+
+$('.textbox_slovar').keyup(function(){
+	var api = "dict.1.1.20191116T165611Z.3a08bd328ca58709.d98fc3f0778c51416b46e13c4f433f53d09ca36d";
+	var url="https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key="+api+"&lang=en-ru&text="+$(this).val();
+	if ($('.textbox_slovar').val() == '') {$('.output').html('<blockquote><p>Не ошибается лишь тот, кто ничего не делает! Не бойтесь ошибаться&nbsp;— бойтесь повторять ошибки.</p><p>Теодор Рузвельт</p></blockquote>');}
+	$.getJSON(url, function(data){
+		$('.output').html('');
+		$('.output').append("<h3>"+data.def[0].text+"</h3>");
+		$('.output').append("<ul>");
+		for (var i = 0; i < 5; i++) {
+			$('.output').append("<li>"+data.def[0].tr[i].text+"</li>");
+		}
+		$('.output').append("</ul>");
+	});
+});
+
